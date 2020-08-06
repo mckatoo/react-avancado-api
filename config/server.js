38224e254609c3ae9e56801f7e39a67d0501ef30
Milton Carlos Katoo <mckatoo@gmail.com>
2020-08-05 21:14:23 -0300

    add default secret key

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET"),
+      secret: env("ADMIN_JWT_SECRET","a969e4b8c87bdcd38b2d1d04fc013a5e"),
     },
   },
 });
