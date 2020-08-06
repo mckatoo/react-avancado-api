c54512827fefe3d880f34b6d9ee2d80b0a3607b7
Milton Carlos Katoo <mckatoo@gmail.com>
2020-08-06 15:53:05 -0300

    change cloudinary for imagekit

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,10 +1,14 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "cloudinary",
+    provider: "imagekit",
     providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET")
+      publicKey: "public_YB9Jm2l/BVdSIZn+Ds2U0j2Dkzc=", // put your publicKey here
+      privateKey: "private_5P92yfWl5Ah8kxM6ZMJlGMQFXvM=", // put your privateKey here
+      urlEndpoint: "https://ik.imagekit.io/povo8thngv", // put your urlEndpoint
+      params: {
+        // optional section
+        folder: "/production/images", // folder location in imagekit.  Defaults to "/" if value is not supplied
+      },
     },
   },
 });
